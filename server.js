779a93904eeffb6974e71c36b69f5ccbd8aa44b7
atollett <a.t.tollett@gmail.com>
2020-09-23 16:00:11 -0700

    api routes, connections, ORM to sequelize

--- server.js
+++ server.js
@@ -9,57 +9,84 @@ var passport = require("./config/passport");
 var db = require("./models");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
-// Middleware
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(bodyParser.json());
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
 app.use(express.static("public"));
-app.use(
-  expressSession({
-    secret: "keyboard cat",
-    resave: true,
-    saveUnintialized: true
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
 
 // Routes
-//require("./routes/apiRoutes")(app, passport);
-//require("./routes/htmlRoutes")(app);
+// =============================================================
+require("./routes/api-routes.js")(app);
 
-var syncOptions = {
-  force: false
-};
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
-
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function () {
-  app.listen(PORT, function () {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
   });
 });
+
+
+
+// var app = express();
+// var PORT = process.env.PORT || 3000;
+
+// // Middleware
+// app.use(bodyParser.urlencoded({
+//   extended: false
+// }));
+// app.use(bodyParser.json());
+// app.use(express.static("public"));
+// app.use(
+//   expressSession({
+//     secret: "keyboard cat",
+//     resave: true,
+//     saveUnintialized: true
+//   })
+// );
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+
+// // Handlebars
+// app.engine(
+//   "handlebars",
+//   exphbs({
+//     defaultLayout: "main"
+//   })
+// );
+// app.set("view engine", "handlebars");
+
+// // Routes
+// //require("./routes/apiRoutes")(app, passport);
+// //require("./routes/htmlRoutes")(app);
+
+// var syncOptions = {
+//   force: false
+// };
+
+// // If running a test, set syncOptions.force to true
+// // clearing the `testdb`
+// if (process.env.NODE_ENV === "test") {
+//   syncOptions.force = true;
+// }
+
+// // Starting the server, syncing our models ------------------------------------/
+// db.sequelize.sync(syncOptions).then(function () {
+//   app.listen(PORT, function () {
+//     console.log(
+//       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+//       PORT,
+//       PORT
+//     );
+//   });
+// });
 
 module.exports = app;
