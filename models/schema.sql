9d3b6c2edb796f566f579ba53e0637dbde7b8937
Aiya Siddig <65461462+aiyasiddig@users.noreply.github.com>
2020-09-24 18:56:13 -0700

    Add files via upload

---
+++ models/schema.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS trivia;
+CREATE database trivia;
+
+USE trivia;
+
+CREATE TABLE trivia_questions (
+  topic VARCHAR(100) NOT NULL,
+  question VARCHAR(100) NOT NULL,
+  optionA VARCHAR(100) NOT NULL,
+  optionB VARCHAR(100) NOT NULL,
+  optionC VARCHAR(100) NOT NULL,
+  optionD VARCHAR(100) NOT NULL,
+  correct_option VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE highscores (
+    id INT NOT NULL,
+    player_topic VARCHAR(100) NOT NULL,
+    player_name VARCHAR(100) NOT NULL,
+    player_score VARCHAR(100) NOT NULL,
+);
---
+++ public/questions.js
@@ -0,0 +1,52 @@
+var mysql = require("mysql");
+var questions = [];
+var connection;
+var topic; //get from somewhere
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWRD,
+    database: "trivia_db"
+  });
+} 
+
+connection.connect(function(err) {
+  if (err) {
+    throw err;
+  }
+  pickQuestions(topic);
+});
+
+function pickQuestions(topic) {
+  connection.query("SELECT * FROM questions WHERE topic = '" + topic + "' ORDER BY RAND() LIMIT 10", function(err, results){
+    if (err) {
+      throw err;
+    }
+    populateQuestions(results);
+  });
+}
+
+function populateQuestions(results) {
+  for (var i = 0; i < results.length; i++) {
+    var question = {};
+    question.topic = results[i].topic;
+    question.question = results[i].question;
+    question.A = results[i].optionA;
+    question.B = results[i].optionB;
+    question.C = results[i].optionC;
+    question.D = results[i].optionD;
+    question.correct = results[i].correct_option;
+    questions.push(question);
+  }
+  if (questions.length === 10) {
+    connection.end();
+  }
+}
+
+module.exports = questions;
+// module.exports = connection;
