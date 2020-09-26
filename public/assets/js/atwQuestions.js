d1f5da1fe583884e2e9d20f2e69d3d59ae304abd
Benjamin Young <ben@benjaminthebroker.com>
2020-09-25 20:06:38 -0700

    add

--- public/assets/js/atwQuestions.js
+++ public/assets/js/atwQuestions.js
@@ -1,14 +1,36 @@
 var questionArray = [{
 
-    question: 'Which NBA player is represented in the NBA logo?',
-    choices: ['Jerry West', 'Kobe Bryant', 'Shaq', 'Karl Malone', ],
-    answer: 'Jerry West',
+    
+    question: 'Which Country has the longest coastline in the world?',
+    choices:['USA',	'Brazil','Morocco',	'Canada'],
+    answer:'Canada',
 
 },
-{
-    question: 'What year was Tom Brady drafted?',
-    choices: ['1999', '2005',	'1980',	'2000', ],
-    answer: '2000',
-}
+{    
+    question: 'Which country holds the worlds largest population?',
+    choices: ['India','Iran','China','Mexico',],
+    answer: 'China',
+    
+},
+{  
+    question: 'Which country does the cultural dance Dabke originate from?',
+    choices: ['Arabic','Fulani','Swahili','Amharic',],
+    answer:'Arabic',
 
+},
+{    
+    question: 'After the Nile, what is the longest river in the world?',
+    choices:['Iran',	'Palestine',	'Portugal',	'Nigeria' ],
+    answer: 'Palestine',
+    
+},
+{       
+    question: 'In which country is it a taboo to write someone’s name in red?',
+    choices:['Amazon',	'Mississippi',	'Yangtze',	'Colombia' ],
+    answer:'Amazon',
+}
 ];
+
+
+
+
--- public/assets/js/politicsQuestions.js
+++ public/assets/js/politicsQuestions.js
@@ -1,14 +1,35 @@
 var questionArray = [{
 
-    question: 'Which NBA player is represented in the NBA logo?',
-    choices: ['Jerry West', 'Kobe Bryant', 'Shaq', 'Karl Malone', ],
-    answer: 'Jerry West',
-
+    question: 'American politician and diplomat Madeleine Albright was born in what central European country?',
+    choices: ['Germany','Poland','Serbia','Czechoslovakia',],
+    answer: 'Czechoslovakia',
 },
 {
-    question: 'What year was Tom Brady drafted?',
-    choices: ['1999', '2005',	'1980',	'2000', ],
-    answer: '2000',
+    question: 'At the time of his assassination, what play was Abraham Lincoln watching?]',
+    choices: ['Carmen',	'Our American Cousin','Uncle Vanya','Mary Stuart',],
+    answer: 'Our American Cousin',
+},
+{  
+    question: 'Where was Richard Nixon when he delivered his infamous “I am not a crook” speech?',
+    choices: ['Portland, Oregon	Washington', 'D.C.','Orlando, Florida',	'New York, New York'],
+    answer: 'Orlando,florida',
+},
+{
+    question: 'In 1952, Albert Einstein was offered the presidency of which country?',
+    choices: ['Israel',	'Germany','England','America'],
+    answer: 'Israel',
+},
+{
+    question: 'Who was first U.S. president to be impeached?',
+    choices:[ 'Andrew Johnson',	'Donald Trump',	'Richard Nixon','Bill Clinton',],
+    answer: 'Andrew Johnson',
+
 }
 
 ];
+
+
+
+
+
+
